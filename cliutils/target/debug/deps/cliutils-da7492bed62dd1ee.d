f263ca269857982c5667f406c0e51c3a24ee85c9
skhshths <oliverbarnet441@gmail.com>
2026-07-29 14:43:04 -0400

    B0.2.1

--- cliutils/target/debug/deps/cliutils-da7492bed62dd1ee.d
+++
@@ -1,7 +0,0 @@
-C:\Users\Student\Desktop\OliverB\rust\cliutils\cliutils\target\debug\deps\cliutils-da7492bed62dd1ee.d: src\lib.rs
-
-C:\Users\Student\Desktop\OliverB\rust\cliutils\cliutils\target\debug\deps\libcliutils-da7492bed62dd1ee.rlib: src\lib.rs
-
-C:\Users\Student\Desktop\OliverB\rust\cliutils\cliutils\target\debug\deps\libcliutils-da7492bed62dd1ee.rmeta: src\lib.rs
-
-src\lib.rs:
--- cliutils/target/debug/libcliutils.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Student\Desktop\OliverB\rust\cliutils\cliutils\target\debug\libcliutils.rlib: C:\Users\Student\Desktop\OliverB\rust\cliutils\cliutils\target\package\cliutils-0.2.0\src\lib.rs
