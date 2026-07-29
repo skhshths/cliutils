ed4765384b0979272de5a0ee5bedd80f22ec90d0
skhshths <oliverbarnet441@gmail.com>
2026-07-29 10:51:28 -0400

    B0.1.7

--- cliutils/target/package/cliutils-0.1.7/src/lib.rs
+++ cliutils/target/package/cliutils-0.1.7/src/lib.rs
@@ -1,8 +1,6 @@
 use std::io;
 use std::io::Write;
 
-use std::collections::HashMap
-
 use std::fmt::Display;
 
 use std::cmp::PartialEq;
---
+++ cliutils/target/package/cliutils-0.1.7/target/debug/deps/cliutils-e75a467fa683f855.d
@@ -0,0 +1,7 @@
+C:\Users\Student\Desktop\OliverB\rust\cliutils\cliutils\target\debug\deps\cliutils-e75a467fa683f855.d: src\lib.rs
+
+C:\Users\Student\Desktop\OliverB\rust\cliutils\cliutils\target\debug\deps\libcliutils-e75a467fa683f855.rlib: src\lib.rs
+
+C:\Users\Student\Desktop\OliverB\rust\cliutils\cliutils\target\debug\deps\libcliutils-e75a467fa683f855.rmeta: src\lib.rs
+
+src\lib.rs:
