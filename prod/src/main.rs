4f4b68618085b68a0d96fbe8119d7d4c0f6c0de8
skhshths <oliverbarnet441@gmail.com>
2026-07-30 15:49:48 -0400

    U0.2.4

--- prod/src/main.rs
+++ prod/src/main.rs
@@ -1,16 +1,23 @@
-mod lib;
+#![allow(dead_code, unused_imports, unused_variables, special_module_name)]
 
-use lib::input;
-use lib::index;
+mod testlib;
 
-use lib::show_vec;
+use std::collections::HashMap;
 
-use lib::clear;
-use lib::clear_win;
+use testlib::vec_to_string;
+use testlib::hash_to_vec;
+use testlib::clear_win;
 
 fn main() {
     let x: Vec<i32> = vec![1, 2, 3, 4, 5];
 
-    let n: String = show_vec(&x, " - ");
-    println!("{n}")
+    let mut b: HashMap<&str, i32> = HashMap::new();
+    b.insert("oliver", 100);
+    b.insert("blaise", 5000);
+
+    let n: Vec<String> = hash_to_vec(&b, " - ");
+
+    for x in n {
+        println!("{x}");
+    }
 }
